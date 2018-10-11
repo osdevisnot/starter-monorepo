accfd3b4f3c324bc4a941d0f00b473372bad7d19
osdevisnot <osdevisnot@gmail.com>
2018-10-10 23:24:04 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# starter-monorepo
+
+This project is a starter for monorepo based applications. To use this starter, ensure you have `degit` installed.
+
+```
+yarn global add degit
+```
+
+To create a new project based on this template:
+
+```
+degit osdevisnot/starter-monorepo my-repo
+cd my-repo
+```
+
+and then run
+
+```
+node setup.js
+```
+
+or for short:
+
+```
+degit osdevisnot/starter-monorepo my-repo && $_ && node setup.js
+```
---
+++ setup.js
@@ -0,0 +1,52 @@
+/**
+ * Simple setup script to be executed after degit finished cloning
+ * Note: Shell commands, Writing files manually should be avoided.
+ */
+const path = require('path')
+const fs = require('fs')
+const sync = require('child_process').execSync
+
+/**
+ * Last portion of working directory...
+ */
+const cwd = path.join(__dirname)
+const name = cwd.split(path.sep).pop()
+
+const fromRoot = file => path.join(cwd, file)
+
+/**
+ * Write a README.md file
+ */
+
+fs.writeFileSync(fromRoot('README.md'), `#${name}\n\n`, 'utf-8')
+
+/**
+ * ReWrite a package.json file
+ * Change package name and remove devDependencies while re-writing
+ */
+
+const pkg = require('./package.json')
+pkg.name = name
+const deps = Object.keys(pkg.devDependencies).join(' ')
+delete pkg.devDependencies
+fs.writeFileSync(fromRoot('package.json'), JSON.stringify(pkg, null, '  ') + '\n', 'utf-8')
+
+/**
+ * Remove Files and Self destruct
+ */
+const files = [fromRoot('applications/.gitkeep'), fromRoot('setup.js')]
+files.forEach(file => fs.unlinkSync(file))
+
+/**
+ * Add latest devDependencies and initialize git repo
+ */
+const commands = [
+  `yarn add --dev ${deps} --ignore-workspace-root-check`,
+  'git init',
+  'git add .',
+  'git commit -am "first commit"'
+]
+commands.forEach(command => {
+  console.log(`----- Executing Command -----> ${command}`)
+  sync(command, { stdio: [0, 1, 2] })
+})
